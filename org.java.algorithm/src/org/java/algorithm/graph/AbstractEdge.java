43e434f6ef34f2330406d45e3982dda7e7d32d52
Han++ <jiahanwang1990@gmail.com>
2014-04-16 21:12:39 -0700

    Revert "SimpleGraph TestCase Added"
    This reverts commit ee43c8fe9655e1dcdd8bcb2fac67a63ca0e47549.

--- org.java.algorithm/src/org/java/algorithm/graph/AbstractEdge.java
+++ org.java.algorithm/src/org/java/algorithm/graph/AbstractEdge.java
@@ -1,19 +1,5 @@
 package org.java.algorithm.graph;
 
 public abstract class AbstractEdge {
-	
-	private String id;
-
-	AbstractEdge(String id){
-		this.id = id;
-	}
-	
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
 
 }
