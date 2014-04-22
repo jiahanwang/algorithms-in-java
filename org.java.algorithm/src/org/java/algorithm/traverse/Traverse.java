7f5587ccddc71937930550d379240e9a2a476f7c
Han++ <jiahanwang1990@gmail.com>
2014-04-22 00:12:23 -0700

    Added topological order of DAG

--- org.java.algorithm/src/org/java/algorithm/traverse/Traverse.java
+++ org.java.algorithm/src/org/java/algorithm/traverse/Traverse.java
@@ -1,6 +1,7 @@
 package org.java.algorithm.traverse;
 
 import org.java.algorithm.graph.*;
+
 import java.util.*;
 
 
@@ -31,7 +32,8 @@ public class Traverse {
 		LinkedList<V> memory = new LinkedList<V>();
 		memory.addLast(src);
 		// if DFS, reverse the comparator
-		if(indicator == TraversalKind.DFS){
+		if(comparator != null && indicator == TraversalKind.DFS){
+			// If the specified comparator is null, it returns a comparator that imposes the reverse of the natural ordering 
 			comparator  = Collections.reverseOrder(comparator);
 		}
 		while(!memory.isEmpty()){
@@ -57,9 +59,12 @@ public class Traverse {
 				}
 			}
 		}
-		return Collections.unmodifiableList(traversalList);
+		return traversalList;
 	}
 	
+	/* public static <V, E> List<List<V>> bfsInLevels(Graph<V, E> graph, V src, V des, Comparator<? super V > comparator){
+		return null;//traverse(graph, src, des, comparator, TraversalKind.BFS);
+	}*/
 	
 	public static <V, E> List<V> bfs(Graph<V, E> graph, V src, V des, Comparator<? super V > comparator){
 		return traverse(graph, src, des, comparator, TraversalKind.BFS);
@@ -74,7 +79,7 @@ public class Traverse {
 	}
 	
 	public static <V, E> List<V> bfs(Graph<V, E> graph, V source){
-		return bfs(graph, source);
+		return bfs(graph, source, null, null);
 	}
 	
 	public static <V, E> List<V> dfs(Graph<V, E> graph, V src, V des, Comparator<? super V > comparator){
@@ -90,67 +95,69 @@ public class Traverse {
 	}
 	
 	public static <V, E> List<V> dfs(Graph<V, E> graph, V source){
-		return dfs(graph, source);
+		return dfs(graph, source, null, null);
 	}
 	
+	/**
+	 * Get the topological order of a directed acyclic graph (DAG)
+	 * 
+	 * @param graph the DAG you want get topological order
+	 * @param comparator  how you want choose vertices when traversing. 
+	 * {@code null} indicates natural ordering is used, which is smaller one will be chosen first
+	 * this comparator is used when traversal needs to choose from vertices.
+	 * @return A list of topological order
+	 * @see #topologicalOrder(DirectedGraph)
+	 * */
+	public static <V, E> List<V> topologicalOrder(DirectedGraph<V, E> graph, Comparator<? super V > comparator){
+		if(graph == null){
+			throw new NullPointerException("Graph or source vertex is null");
+		}
+		// need extra space to store marks for traversal, boolean for instance 
+		Map<V, Boolean> traversalMarks = new HashMap<V, Boolean>();
+		// declare the list to be returned
+		LinkedList<V> traversalList = new LinkedList<V>();
+		// declare the list to hold potential roots
+		List<V> rootList = new LinkedList<V>();
+		for(V vertex : graph.getVertices()){
+			traversalMarks.put(vertex, false);
+			if(graph.inDegree(vertex) == 0){
+				rootList.add(vertex);
+			}
+		}
+		Collections.sort(rootList, comparator);
+		for(V root : rootList){
+			topological_recursive(graph, root, comparator, traversalMarks, traversalList);
+		}
+		return traversalList;
+	} 
 	
-	/*public void dfs_recursive(Graph<V, E> graph, V root, Collection<V> traversalList){
+	/**
+	 * Get the topological order of a directed acyclic graph (DAG). Natural ordering is used when traversal needs to choose from vertices.
+	 * 
+	 * @param graph the DAG you want get topological order
+	 * @return A list of topological order
+	 * @see #topologicalOrder(DirectedGraph, Comparator)
+	 * */
+	public static <V, E> List<V> topologicalOrder(DirectedGraph<V, E> graph){
+		return topologicalOrder(graph, null);
+	} 
+	
+	private static <V, E> void topological_recursive(Graph<V, E> graph, V root, Comparator<? super V > comparator, Map<V, Boolean> traversalMarks, LinkedList<V> traversalList){
 		if(traversalMarks.get(root) == true){
 			return;
 		}else{
 			traversalMarks.put(root, true);
-			traversalList.add(root);
 		}
 		List<V> adjacentVertices = new LinkedList<V>(graph.adjacentVertices(root));
-		if(comparator != null)
+		if(comparator != null){
 			Collections.sort(adjacentVertices, comparator);
+		}
 		for(V vertex : adjacentVertices){
 			if(traversalMarks.get(vertex) == false){
-				dfs_recursive(graph, vertex, traversalList);
+				topological_recursive(graph, vertex, comparator, traversalMarks, traversalList);
 			}
 		}
-	}*/
-	
-	
-	public static void main(String[] args){
-		SimpleGraph<String, String> graph = new SimpleGraph<String, String>();
-		graph.addEdge("edge1-2", "1", "2");
-		graph.addEdge("edge1-3", "1", "3");
-		graph.addEdge("edge2-3", "2", "3");
-		graph.addEdge("edge3-4", "3", "4");
-		graph.addVertex("5");
-		graph.addEdge("edge1-5", "1", "5");
-		graph.addVertex("6");
-		
-		System.out.println(Traverse.bfs(graph, "1", new Comparator<String>(){
-			public int compare(String a, String b){
-				return a.compareTo(b);
-			}
-		}));
-		
-		System.out.println(Traverse.dfs(graph, "1", new Comparator<String>(){
-			public int compare(String a, String b){
-				return a.compareTo(b);
-			}
-		}));
-		
-		BFSIterator<String, String> iterator = new BFSIterator<String, String>(graph, "1", new Comparator<String>(){
-			public int compare(String a, String b){
-				return a.compareTo(b);
-			}
-		});
-		for(;iterator.hasNext();){
-			System.out.println(iterator.next());
-		}
-		
-		DFSIterator<String, String> iterator2 = new DFSIterator<String, String>(graph, "1", new Comparator<String>(){
-			public int compare(String a, String b){
-				return a.compareTo(b);
-			}
-		});
-		for(;iterator2.hasNext();){
-			System.out.println(iterator2.next());
-		}
+		traversalList.addFirst(root);
 	}
-	
+		
 }
