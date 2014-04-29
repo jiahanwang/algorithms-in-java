224afd190ab6cff23793ebab80f008481b33e185
Han++ <jiahanwang1990@gmail.com>
2014-04-29 13:05:49 -0700

    Optimized MergeSort, reduced extra space to n/2

--- org.java.algorithm/src/org/java/algorithm/sorting/MergeSort.java
+++ org.java.algorithm/src/org/java/algorithm/sorting/MergeSort.java
@@ -11,12 +11,12 @@ public class MergeSort {
 	 * 
 	 * 
 	 * */
-	public static <T> Collection<T> mergeSort(Collection<T> values, Comparator<? super T> c){
-		return mergeSort(values, 0, values.size() - 1, c);
-	}
-	
 	public static <T> Collection<T> mergeSort(Collection<T> values){
 		return mergeSort(values, 0, values.size() - 1, null);
+	}
+	
+	public static <T> Collection<T> mergeSort(Collection<T> values, Comparator<? super T> c){
+		return mergeSort(values, 0, values.size() - 1, c);
 	}
 	
 	public static <T> Collection<T> mergeSort(Collection<T> values, int fromIndex, int toIndex){
@@ -40,12 +40,12 @@ public class MergeSort {
 	 * 
 	 * 
 	 * */
-	public static <T> void mergeSort(T[] values, Comparator<? super T> c){
-		mergeSort(values, 0, values.length - 1, c);
-	}
-	
 	public static <T> void mergeSort(T[] values){
 		mergeSort(values, 0, values.length - 1, null);
+	}
+	
+	public static <T> void mergeSort(T[] values, Comparator<? super T> c){
+		mergeSort(values, 0, values.length - 1, c);
 	}
 	
 	public static <T> void mergeSort(T[] values, int fromIndex, int toIndex){
@@ -59,7 +59,8 @@ public class MergeSort {
 		if(fromIndex > toIndex || fromIndex < 0 || toIndex > values.length - 1){
 			throw new IllegalArgumentException("fromIndex or toIndex is wrong");
 		}
-		T[] helper = (T[])(new Object[values.length]);
+		// need a helper array with size of length / 2 + 1, when merging copy left half into helper
+		T[] helper = (T[])(new Object[values.length / 2 + 1]);
 		mergeSort_recursive(values, helper, fromIndex, toIndex, c);
 	}
 	
@@ -69,45 +70,26 @@ public class MergeSort {
 	 * */
 	@SuppressWarnings("unchecked")
 	private static <T> void mergeSort_recursive(T[] values, T[] helper, int fromIndex, int toIndex, Comparator<T> c){
-		if(values.length > 1 && fromIndex < toIndex){
-			int middleIndex = fromIndex + (toIndex - fromIndex) / 2;
-			mergeSort_recursive(values, helper, fromIndex ,middleIndex, c);
-			mergeSort_recursive(values, helper, middleIndex + 1, toIndex, c);
-			// copy values[] to helper[], values[] is the final result
-			for(int i = fromIndex; i <= toIndex; i++){
-				helper[i] = values[i];
+		//when fromIndex == toIndex return, or stack overflow
+		if(fromIndex >= toIndex) return;
+		int middleIndex = fromIndex + (toIndex - fromIndex) / 2;
+		mergeSort_recursive(values, helper, fromIndex ,middleIndex, c);
+		mergeSort_recursive(values, helper, middleIndex + 1, toIndex, c);
+		int leftLen = middleIndex - fromIndex + 1;
+		// copy values[] to helper[], values[] is the final result
+		System.arraycopy(values, fromIndex, helper, 0, leftLen);
+		// merge two halves to values[]
+		int i = 0, j = middleIndex + 1, k = fromIndex;
+		for(; k <= toIndex;){
+			// if all elements in helper have been moved to values, then values is sorted
+			if(i >= leftLen) break;
+			if(c != null){
+				values[k++] = ( j > toIndex || c.compare(helper[i], values[j]) <= 0 )? helper[i++] : values[j++];
 			}
-			// remove to values[]
-			int i = fromIndex, j = middleIndex + 1, k = fromIndex;
-			for(; i <= middleIndex && j <= toIndex;){
-				if(c != null){
-					values[k++] = c.compare(helper[i], helper[j]) <= 0 ? helper[i++] : helper[j++];
-				}
-				else{
-					values[k++] = ((Comparable)helper[i]).compareTo(helper[j]) <= 0 ? helper[i++] : helper[j++];
-				}
-			}
-			// remove rest to values[]
-			if(i > middleIndex){
-				while(j <= toIndex)
-					values[k++] = helper[j++];
-			}else{
-				while(i <= middleIndex)
-					values[k++] = helper[i++];
+			else{
+				values[k++] = ( j > toIndex || ((Comparable)helper[i]).compareTo(values[j]) <= 0 ) ? helper[i++] : values[j++];
 			}
 		}
 	}
-	
-	/*public static void main(String[] args){
-		Integer[] array = new Integer[]{4, 5, 2, 5, 7, 2, 4, 99, 22, 66};
-		LinkedList<Integer> list = new LinkedList<Integer>();
-		list.addAll(Arrays.asList(array));
-		MergeSort.mergeSort(array, 1, 6);
-		for(Integer i : array){
-			System.out.println(i);
-		}
-		System.out.println(MergeSort.mergeSort(list, 7, 6));
-		
-	}
-*/
+
 }
